52147f01e79bb4b6986d9ef8a09886861088134c
elementemerald <22267679+elementemerald@users.noreply.github.com>
2020-07-14 19:02:47 -0600

    https package

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const checker = require("./checker.js");
 
 app.get("/", function (req, res) {
     res.send("Simple HTTPS Checker made by elementemerald#0002");
+});
+
+app.get("/check", function (req, res) {
+    
 });
