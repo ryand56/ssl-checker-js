0b3ab84cbbcc4d56d8fc5442d976153eb328dc34
elementemerald <22267679+elementemerald@users.noreply.github.com>
2020-07-14 19:21:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/check", function (req, res) {
+    if (!req.query.url) return res.status(400).json({status: "err", message: "URL parameter not found"});
     if (req.query.url.includes("https://")) return res.json({status: "warn", message: "Protocol not needed"});
     checker.get(req.query.url).then(function (cert) {
         res.json({status: "ok", issuer: cert.issuer.CN});
