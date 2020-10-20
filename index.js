ee0735911d931faa808f0cb9d547cb619cd4856b
Ryan <22267679+elementemerald@users.noreply.github.com>
2020-10-20 11:57:46 -0600

    update tag

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const checker = require("./checker.js");
 
 app.get("/", function (req, res) {
-    res.send("Simple HTTPS Checker made by elementemerald#0002");
+    res.send("Simple HTTPS Checker made by elementemerald#4175");
 });
 
 app.get("/check", function (req, res) {
